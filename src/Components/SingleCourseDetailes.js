28f613c1146260029983af8182b338af86ae2255
Mashud05052001 <masudmahi0505@gmail.com>
2022-10-27 03:56:37 +0600

    finished and pdf done but unable to capture full page pdf

--- src/Components/SingleCourseDetailes.js
+++ src/Components/SingleCourseDetailes.js
@@ -2,20 +2,30 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaCheck } from "react-icons/fa";
 import { GiChessKing } from "react-icons/gi";
-
+import { FaDownload } from "react-icons/fa";
+import Pdf from "react-to-pdf";
 
 
 const SingleCourseDetailes = () => {
     const information = useLoaderData();
     const { _id, detailes, image, tools, name } = information;
-
+    const ref = React.createRef();
     return (
         <section className='mx-auto'>
 
             <div className='min-h-[90.1vh] flex items-center bg-no-repeat bg-cover' style={{ backgroundImage: `url(${image})` }}>
                 <div className=' text-white'>
-                    <div className='border-2  lg:w-10/12 w-[90%] mx-auto lg:rounded-3xl rounded-xl  bg-black/80'>
+                    <div ref={ref} className='relative border-2  lg:w-10/12 w-[90%] mx-auto lg:rounded-3xl rounded-xl  bg-black/80'>
                         <h1 className='font-[serif] text-3xl lg:text-5xl font-bold text-center text-white mt-3'>{name}</h1>
+                        <div className='absolute right-6 top-5'>
+                            <button className='btn tooltip tooltip-top' data-tip="I tried so much but could not capture full page pdf">
+                                <Pdf targetRef={ref} filename="code-example.pdf">
+                                    {({ toPdf }) => <button onClick={toPdf} className='flex items-center'>Download PDF
+                                        <FaDownload className='ml-3' />
+                                    </button>}
+                                </Pdf>
+                            </button>
+                        </div>
                         <aside className='flex flex-col lg:flex-row mt-5 gap-x-10 font-medium' >
                             <p className='lg:w-6/12 text-justify leading-8 px-4 pb-8 lg:mt-5 mt-0'>{detailes}</p>
                             <div className='pr-3 py-5'>
